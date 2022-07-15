9bfc1552ec2e78d15acf239560df4adc5e7a7fec
satinder <satindersingh472@gmail.com>
2022-07-14 19:01:36 -0600

    finished

--- js/app.js
+++ js/app.js
@@ -10,3 +10,19 @@ while(counter < all_classes.length){
 all_classes[counter][`innerText`] = `These were all same class`;
 counter ++ ;
 }
+
+let third_id_tag = document.getElementById(`third_id`);
+third_id_tag[`innerText`] = `This was the third id tag`;
+
+let fourth_id_tag = document.getElementById(`fourth_id`);
+fourth_id_tag[`innerText`] = `This was the fourth id`;
+
+let fifth_id_tag = document.getElementById(`fifth_id`);
+fifth_id_tag[`innerText`]  = `This was the fifth id`;
+
+let all_other_classes = document.getElementsByClassName(`other_class`);
+let other_counter = 0;
+while(other_counter < all_classes.length){
+    all_other_classes[other_counter][`innerText`] = `These were all other classes`;
+    other_counter++;
+}
